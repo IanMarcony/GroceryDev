41aafa577398e2e93f5af279f57f7fd1525fc110
IanMarcony <ianmarconysalazar@outlook.com>
2021-03-31 19:49:12 -0400

    Refactored  code on backend

---
+++ backend/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': './src/modules',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
